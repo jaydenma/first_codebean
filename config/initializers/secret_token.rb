ead64efbb69786c5f4486322ef9e37fcdf6f8103
Jayden <jayden@InfinityDrive.(none)>
2012-10-08 11:19:21 +0800

    First initial test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstCodebean::Application.config.secret_token = '88a22e41f40f1e45f60966482bfc20c347996875fc1c1610a5acc3f2fde88e02873bcb8f5e4db4a817964d3141c192bfa404c73fcd80203dd0f518d0f2015654'
