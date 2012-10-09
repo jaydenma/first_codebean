816e8e7b369dcf8dc8df5f7487e176f42675e4eb
Jayden Ma <jayden@codebean.sg>
2012-10-09 13:27:51 +0800

    Added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 FirstCodebean::Application.routes.draw do
+  get "users/new"
+
 	root :to => 'pages#home'
 	
 	match '/contact', :to => 'pages#contact'
 	match '/about', :to => 'pages#about'
+	match '/signup', :to => 'users#new'
 	
   # The priority is based upon order of creation:
   # first created -> highest priority.
