a0e689025e48d3a5e28826eaac0f329870850953
Jayden Ma <jayden@codebean.sg>
2012-10-09 16:18:09 +0800

    User validation is in

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@
 
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+  
+  email_regex = /^([0-9a-zA-Z]([-+\.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-+\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/i
+  
+  validates :name,  :presence => true,
+  									:length   => { :maximum => 50 }
+  validates :email, :presence   => true,
+  									:format		  => { :with => email_regex },
+  									:uniqueness => { :case_sensitive => false }
 end
