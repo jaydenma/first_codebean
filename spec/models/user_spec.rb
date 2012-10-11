28f5e43145f7a79bc52aba782e34fbe7295028fa
Jayden Ma <jayden@codebean.sg>
2012-10-11 14:54:35 +0800

    Added destroy users for admins

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,8 @@
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #  encrypted_password :string(255)
+#  salt               :string(255)
+#  admin              :boolean          default(FALSE)
 #
 
 require 'spec_helper'
@@ -160,5 +162,26 @@ describe User do
 	 			User.authenticate(@attr[:email], @attr[:password]).should == @user
 	 		end
 	 	end
+ 	end
+ 	
+ 	describe "admin attribute" do
+ 		
+ 		before(:each) do
+ 			@user = User.create!(@attr)
+ 		end
+ 		
+ 		it "should respond to admin" do
+ 			@user.should respond_to(:admin)
+ 		end
+ 		
+ 		it "should not be an admin by default" do
+ 			@user.should_not be_admin
+ 		end
+ 		
+ 		it "should be convertible to an admin" do
+ 			@user.toggle!(:admin)
+ 			@user.should be_admin
+ 		end
+ 		
  	end
 end
