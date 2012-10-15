9ea04710593a1ba49f140cb8649a2a3f6b4f373f
Jayden Ma <jayden@codebean.sg>
2012-10-15 16:33:54 +0800

    Initial interface for following/followers

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,21 @@
 FirstCodebean::Application.routes.draw do
 
-	resources :users
-	resources :sessions, 	 :only => [:new, :create, :destroy]
-	resources :microposts, :only => [:create, :destroy]
+	resources :users do
+		member do
+			get :following, :followers
+		end
+	end
+	
+	resources :sessions, 		  :only => [:new, :create, :destroy]
+	resources :microposts, 		:only => [:create, :destroy]
+	resources :relationships, :only => [:create, :destroy]
 
 	root :to => 'pages#home'
 	
 	match '/contact', :to => 'pages#contact'
-	match '/about', :to => 'pages#about'
-	match '/signup', :to => 'users#new'
-	match '/signin', :to => 'sessions#new'
+	match '/about', 	:to => 'pages#about'
+	match '/signup',  :to => 'users#new'
+	match '/signin',  :to => 'sessions#new'
 	match '/signout', :to => 'sessions#destroy'
 	
   # The priority is based upon order of creation:
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -4,22 +4,48 @@ describe PagesController do
 	render_views
 	
   describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
-    end
+  
+  	describe "when not signed in" do
+  	
+		  it "returns http success" do
+		    get 'home'
+		    response.should be_success
+		  end
 
-		it "should have the right title" do
-			get 'home'
-			response.should have_selector("title", :content => "Home")			
+			it "should have the right title" do
+				get 'home'
+				response.should have_selector("title", :content => "Home")			
+			end
 		end
-  end
 
+		describe "when signed in" do
+		
+  		before(:each) do
+  			@user = test_sign_in(Factory(:user))
+  			other_user = Factory(:user, :email => Factory.next(:email))
+  			other_user.follow!(@user)
+  		end
+  		
+  		it "should have the right follower/following counts" do
+  			get :home
+  			response.should have_selector('a', :href => following_user_path(@user),
+  																				 :content => "0 following")
+  			response.should have_selector('a', :href => followers_user_path(@user),
+  																				 :content => "1 follower")
+  		end
+  	end
+  end
+  
   describe "GET 'contact'" do
     it "returns http success" do
       get 'contact'
       response.should be_success
     end
+    
+		it "should have the right title" do
+			get 'contact'
+			response.should have_selector("title", :content => "Contact")			
+		end
   end
 
   describe "GET 'about'" do
@@ -27,6 +53,11 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    
+		it "should have the right title" do
+			get 'about'
+			response.should have_selector("title", :content => "About")			
+		end
   end
 
 end
